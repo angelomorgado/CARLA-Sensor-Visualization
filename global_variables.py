834c908ff8445fb6f710d95d90eeb8c16cf4e5cc
AmrMorgado <angelo.morgado@ubi.pt>
2024-02-02 12:47:45 +0000

    code formatting

--- global_variables.py
+++ global_variables.py
@@ -19,27 +19,10 @@ VERBOSE     = False
 VEHICLE     = "vehicle.tesla.model3"
 
 # Sensor list so they are stored in a persistent data structure to remain active and to be properly destroyed
-SENSOR_DICT = {
-    'rgb_camera': None,
-    'lidar': None,
-    'radar': None,
-    'gnss': None,
-    'imu': None,
-    'collision': None,
-    'lane_invasion': None
-}
+SENSOR_DICT = {}
 
 # Dict with the last data from each sensor
-DATA_DICT = {
-    'rgb_camera': None,
-    'lidar': None,
-    'radar': None,
-    'gnss': None,
-    'imu': None,
-    'collision': None,
-    'lane_invasion': None
-}
+DATA_DICT = {}
 
-NON_DISPLAYABLE_SENSORS = ['gnss', 'imu','collision', 'lane_invasion']
 # Dict holding the pygame subsurfaces for each displayable sensor
 SENSOR_WINDOWS = {}
